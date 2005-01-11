fcb1d7df4f1e1b630d2e28a4b2bb2ed76587c86c
filetreefrog <>
2005-01-11 16:11:52 +0000

    Renamed the install.php file to db_recover.php, and added an explicit exit() call at the beginning to disable the script.

--- db_recover.php
+++ db_recover.php
@@ -31,19 +31,37 @@
 # $Id$
 ##################################################
 
+// This script exits intiially by default, to prevent unauthorized access.
+// In the event that you need to recover your database structure, comment
+// out the following line:
+exit('The Database Recovery Script is turned off by default, for security reasons.  To enable it, please edit this PHP file, and uncomment the exit() line.');
+
+// If we made it here, the user has enabled the Database Recovery Script manually.
+
 include_once("pathos.php");
 define("SCRIPT_RELATIVE",PATH_RELATIVE);
 define("SCRIPT_ABSOLUTE",BASE);
 define("SCRIPT_FILENAME","install.php");
 
+// Save the old user data, in case current user is actually logged in.
 $oldu = $user;
-$user->is_admin = 1; // hack the installer
+// Temproarily elevate the current user to admin status, to 
+// allow them to install tables.
+$user->is_admin = 1;
 
+// The $loc variable would normally be created by the Pathos framework
+// when running the action we are about to include.  Here, we synthetically
+// create the location, so that the action doesn't freak out.
 $loc = pathos_core_makeLocation("administrationmodule");
 
+// Simulate running the Install Tables action.
 include_once("modules/administrationmodule/actions/installtables.php");
 
-// Initial setup to get things going.
+// In case something is screwed up in the database, we need to 
+// create some records.
+
+// Create the default administrative account (username:admin, password:admin)
+// if there are no users in the user table.
 if ($db->tableIsEmpty("user")) {
 	echo "Creating default administrator account.<br />";
 	$user = null;
@@ -53,6 +71,10 @@ if ($db->tableIsEmpty("user")) {
 	$db->insertObject($user,"user");
 }
 
+// If no modules have been activated, we will not be able to activate any modules
+// through the Administration Control Panel, usually because we won't
+// be able to add the inactive Admin Control Panel to get access to the module
+// manager.  Activate at least the Administrative Module if there are no modules.
 if ($db->tableIsEmpty("modstate")) {
 	echo "Activating Administration Module by default.<br />";
 	$modstate = null;
@@ -61,6 +83,8 @@ if ($db->tableIsEmpty("modstate")) {
 	$db->insertObject($modstate,"modstate");
 }
 
+// If therer are no sections in the database, we should create a default Home section
+// so that the user at least has a starting page.
 if ($db->tableIsEmpty("section")) {
 	echo "Creating default 'Home' section.<br />";
 	$section = null;
