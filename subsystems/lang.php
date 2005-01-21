6c09eb954f283195dd4bad9a5032e3effcb4f812
filetreefrog <>
2005-01-21 15:34:44 +0000

    Added lang subsystem

---
+++ subsystems/lang.php
@@ -0,0 +1,34 @@
+<?php
+
+define('SYS_LANG',1);
+
+function pathos_lang_list() {
+	$dir = BASE.'subsystems/lang';
+	$langs = array();
+	if (is_readable($dir)) {
+		$dh = opendir($dir);
+		while (($f = readdir($dh)) !== false) {
+			if (substr($f,-4,4) == '.php') {
+				$info = include($dir.'/'.$f);
+				$langs[substr($f,0,-4)] = $info['name'] . ' -- ' . $info['author'];
+			}
+		}
+	}
+	return $langs;
+}
+
+function pathos_lang_initialize() {
+	if (!defined('DEFAULT_VIEW')) {
+		$info = include_once(BASE.'subsystems/lang/'.LANG.'.php');
+		setlocale(LC_ALL,$info['locale']);
+		
+		// For view resolution
+		define('DEFAULT_VIEW',$info['default_view']);
+	}
+}
+
+function pathos_lang_loadDictionary($type,$dictionary) {
+	include_once(BASE.'subsystems/lang/'.LANG.'/'.$type.'/'.$dictionary.'.php'); // Will define constants.
+}
+
+?>
