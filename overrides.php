29c0c5fe0cbc01c9824c7af2eacb09b226014cc4
kessler44 <>
2005-01-21 15:28:38 +0000

    Added comments to the overrides file

--- overrides.php
+++ overrides.php
@@ -1,10 +1,30 @@
 <?php
 
-// Overrides file
-
-// Use this to override path autodetection
-// This file is intended to be editted by the user as a
-// last resort to get things to work around server setup
-// and configuration
+#
+# Overrides File
+# ------------------
+# This file allows you to manually provide
+# definitions of certain auto-detected
+# constants in Exponent.
+# 
+# The following constants can be overridden:
+# 
+# - BASE
+# 	The absolute path of the Exponent
+# 	installation directory.
+#   DO NOT FORGET THE LEADING AND TRAILING '/'s
+#
+#define("BASE","/full/path/to/exponent/");
+#
+#
+#
+#
+# - PATH_RELATIVE
+# 	The path of the website, relative to
+# 	the domain of the URL
+#   DO NOT FORGET THE LEADING AND TRAILING '/'s
+# 
+#define("PATH_RELATIVE","/url/path/to/exponent/");
+#
 
 ?>
