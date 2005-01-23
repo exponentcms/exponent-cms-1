849bc3840daa5af725c60a6917c692f9aa6adfe2
filetreefrog <>
2005-01-23 20:50:25 +0000

    Quote changes and some copyright notices

--- datatypes/category.php
+++ datatypes/category.php
@@ -1,24 +1,55 @@
 <?php
 
+##################################################
+#
+# Copyright 2004 James Hunt and OIC Group, Inc.
+#
+# This file is part of Exponent
+#
+# Exponent is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License as published by the Free
+# Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Exponent is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU
+# General Public License along with Exponent; if
+# not, write to:
+#
+# Free Software Foundation, Inc.,
+# 59 Temple Place,
+# Suite 330,
+# Boston, MA 02111-1307  USA
+#
+# $Id$
+##################################################
+
 class category {
 	function form($object) {
 		pathos_lang_loadDictionary('modules','category');
 		pathos_lang_loadDictionary('standard','core');
 		
-		if (!defined("SYS_FORMS")) include_once(BASE."subsystems/forms.php");
+		if (!defined('SYS_FORMS')) include_once(BASE.'subsystems/forms.php');
 		pathos_forms_initialize();
 		
 		$form = new form();
 		if (!isset($object->id)) {
-			$object->name = "";
-			$object->color = "#000000";
+			$object->name = '';
+			$object->color = '#000000';
 		} else {
-			$form->meta("id",$object->id);
+			$form->meta('id',$object->id);
 		}
 		
-		$form->register("name",TR_CATEGORY_NAME,new textcontrol($object->name));
-		$form->register("color",TR_CATEGORY_COLOR,new textcontrol($object->color));
-		$form->register("submit","",new buttongroupcontrol(TR_CORE_SAVE,"",TR_CORE_CANCEL));
+		$form->register('name',TR_CATEGORY_NAME,new textcontrol($object->name));
+		$form->register('color',TR_CATEGORY_COLOR,new textcontrol($object->color));
+		$form->register('submit','',new buttongroupcontrol(TR_CORE_SAVE,'',TR_CORE_CANCEL));
 		
 		pathos_forms_cleanup();
 		return $form;
