478a772fdc848cb6bf8e6a5b14e37d3357aca221
jshinall_sf <>
2005-01-24 20:21:42 +0000

    Fixed some misspellings in swfmodule.php
    --Jeremy

--- subsystems/lang/en/modules/swfmodule.php
+++ subsystems/lang/en/modules/swfmodule.php
@@ -3,18 +3,16 @@
 // I18n constants for SWF Module
 
 // Permissions
-define('TR_SWFMODULE_PERM_ADMIN',				'Administrate');
-define('TR_SWFMODULE_PERM_CONFIG',				'Configure Flash');
-
-define('TR_SWFMODULE_NAME',						'Name');
-define('TR_SWFMODULE_BGCOLOR',					'Background Color');
-define('TR_SWFMODULE_HEIGHT',					'Flash Height');
-define('TR_SWFMODULE_WIDTH',					'Flash Width');
-define('TR_SWFMODULE_ALIGNEMENT',				'Alignment');
-define('TR_SWFMODULE_FLASHFILE',				'Flash File');
-define('TR_SWFMODULE_ALTIMAGE',					'No Flash<br>Alternative Image');
-
-define('TR_SWFMODULE_KEEPOLDFLASH',				'*Leave Flash File blank to keep existing.');
-define('TR_SWFMODULE_KEEPOLDIMAGE',				'*Leave Alternative Image blank to keep existing.');
+define('TR_SWFMODULE_PERM_ADMIN', 'Administrate');
+define('TR_SWFMODULE_PERM_CONFIG', 'Configure Flash');
+define('TR_SWFMODULE_NAME', 'Name');
+define('TR_SWFMODULE_BGCOLOR', 'Background Color');
+define('TR_SWFMODULE_HEIGHT', 'Flash Height');
+define('TR_SWFMODULE_WIDTH', 'Flash Width');
+define('TR_SWFMODULE_ALIGNMENT', 'Alignment');
+define('TR_SWFMODULE_FLASHFILE', 'Flash File');
+define('TR_SWFMODULE_ALTIMAGE', 'No Flash<br>Alternative Image');
+define('TR_SWFMODULE_KEEPOLDFLASH',	'*Leave Flash File blank to keep existing.');
+define('TR_SWFMODULE_KEEPOLDIMAGE',	'*Leave Alternative Image blank to keep existing.');
 
 ?>
