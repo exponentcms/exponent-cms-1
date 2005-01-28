8a760ab542c431b93cf7ffacac25c04b772d4477
filetreefrog <>
2005-01-28 20:02:28 +0000

    Fixed i18n constant name problem

--- datatypes/formbuilder_form.php
+++ datatypes/formbuilder_form.php
@@ -100,7 +100,7 @@ class formbuilder_form {
 		
 		$form->register('addresses',TR_FORMBUILDER_OTHERADDRESSES,new listbuildercontrol($defaults,array()));
 		$form->register('subject',TR_FORMBUILDER_EMAILSUBJECT,new textcontrol($object->subject));
-		$form->register(null,'', new htmlcontrol('<br><br><b>'.TR_FORBUILDER_DBHEADER.'</b><br><hr size="1"><br>'));
+		$form->register(null,'', new htmlcontrol('<br><br><b>'.TR_FORMBUILDER_DBHEADER.'</b><br><hr size="1"><br>'));
 		$form->register('is_saved',TR_FORMBUILDER_SAVETODB,new checkboxcontrol($object->is_saved,false));
 		$form->register(null,'', new htmlcontrol('<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.TR_FORMBUILDER_DATALOSSWARNING.'<br>'));
 		if ($object->is_saved) {
