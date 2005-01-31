a0fc0c8048bbffbd55b9992eadb181c4b3209b50
filetreefrog <>
2005-01-31 15:13:42 +0000

    Removed javascript solution to PNG bug, in favor of htc for IE

--- pathos.js.php
+++ pathos.js.php
@@ -39,7 +39,6 @@ include_once("pathos.php");
 var onLoadInits = new Array(); // array of functions
 
 function pathosJSinitialize() {
-	correctPNG();
 	for (i = 0; i < onLoadInits.length; i++) {
 		onLoadInits[i]();
 	}
@@ -53,31 +52,6 @@ var PATH_RELATIVE = "<?php echo PATH_RELATIVE; ?>";
 var THEME_RELATIVE = "<?php echo THEME_RELATIVE; ?>";
 var ICON_RELATIVE = "<?php echo ICON_RELATIVE; ?>";
 var SEF_URLS = <?php echo SEF_URLS; ?>;
-
-function correctPNG() // correctly handle PNG transparency in Win IE 5.5 or higher.
-   {
-   for(var i=0; i<document.images.length; i++)
-      {
-	  var img = document.images[i]
-	  var imgName = img.src.toUpperCase()
-	  if (imgName.substring(imgName.length-3, imgName.length) == "PNG")
-	     {
-		 var imgID = (img.id) ? "id='" + img.id + "' " : ""
-		 var imgClass = (img.className) ? "class='" + img.className + "' " : ""
-		 var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
-		 var imgStyle = "display:inline-block;" + img.style.cssText 
-		 if (img.align == "left") imgStyle = "float:left;" + imgStyle
-		 if (img.align == "right") imgStyle = "float:right;" + imgStyle
-		 if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle		
-		 var strNewHTML = "<span " + imgID + imgClass + imgTitle
-		 + " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
-	     + "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
-		 + "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>" 
-		 img.outerHTML = strNewHTML
-		 i = i-1
-	     }
-      }
-   }
 
 function pathosGetCookie(name) {
 	cookiestr = document.cookie;
