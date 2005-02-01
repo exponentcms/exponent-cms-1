0c9f64327c143fd0d3df2e5874e6853612c44c23
filetreefrog <>
2005-02-01 19:48:34 +0000

    Updated blanks template for theme_index files

--- sdk/blanks/theme_index_parts.php
+++ sdk/blanks/theme_index_parts.php
@@ -3,7 +3,7 @@
 	<head>
 		<title><?php echo SITE_TITLE; ?></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-		<meta name="Generator" value="Exponent Content Management System" />
+		<?php echo pathos_theme_metaInfo($section); ?>
 		<link rel="stylesheet" title="default" href="<?php echo THEME_RELATIVE; ?>style.css" />
 		<script type="text/javascript" src="<?php echo PATH_RELATIVE; ?>pathos.js.php"></script>
 	</head>
