1126e839059532a3ce5ef048fb23cb55f02b6598
filetreefrog <>
2005-02-06 20:32:28 +0000

    Added welcome page

---
+++ install/pages/welcome.php
@@ -0,0 +1,10 @@
+<div class="installer_title">
+<img src="images/blocks.png" width="32" height="32" />
+Welcome to the Exponent Install Wizard
+</div>
+<br /><br />
+First, the Exponent Development Team and the OIC Group want to thank you for downloading and installing the Exponent Content Management System.  We fervently hope that you will enjoy the power and flexibility that Exponent has to offer.
+<br /><br />
+This installation wizard will guide you step by step through the configuration and setup of your new Exponent-powered website.
+<br /><br />
+To start the installation, click <a href="install.php?page=sanity">here</a>
