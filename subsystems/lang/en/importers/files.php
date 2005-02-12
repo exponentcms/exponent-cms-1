7602a4fb562abea9ceb52dcf01ea7ac72e698b9a
filetreefrog <>
2005-02-12 02:15:51 +0000

    Added language dictionary for files importer

---
+++ subsystems/lang/en/importers/files.php
@@ -0,0 +1,6 @@
+<?php
+
+define('TR_IMPORTERMODULE_FILES_NAME',			'Uploaded Files Importer');
+define('TR_IMPORTERMODULE_FILES_DESCRIPTION',	'This importer allows you to restore the uploaded files that were backed up with the Uploaded Files Exporter.');
+
+?>
