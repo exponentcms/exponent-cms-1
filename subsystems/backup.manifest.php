ed5830fa4c58ef46e0ce21d93427bbeca039892c
filetreefrog <>
2005-02-13 05:33:41 +0000

    Updated manifest files

--- subsystems/backup.manifest.php
+++ subsystems/backup.manifest.php
@@ -3,6 +3,8 @@
 return array(
 	"subsystems/backup.php"=>"410152fabf287128918409435309d8ac",
 	"subsystems/backup.info.php"=>"",
+	'subsystems/backup/normalize.php'=>'',
+	'subsystems/backup/0.95.php'=>'',
 );
 
 ?>
--- subsystems/image.manifest.php
+++ subsystems/image.manifest.php
@@ -3,6 +3,7 @@
 return array(
 	"subsystems/image.php"=>"",
 	"subsystems/image.info.php"=>"",
+	'subsystems/image/default_preview.gif'=>'',
 );
 
 ?>
