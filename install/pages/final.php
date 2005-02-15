96d92a73e5320163cd20782caf9234fdfcd9e0f9
filetreefrog <>
2005-02-15 00:14:52 +0000

    Removed '_blank' target from 'visit new website' link

--- install/pages/final.php
+++ install/pages/final.php
@@ -59,5 +59,5 @@ You can login to your new website using the following administrator account:
 <tr><td><b>Password:</b></td><td>admin</td></tr>
 </table>
 <br />
-You can visit your new website <a href="http://<?php echo $_SERVER['HTTP_HOST'].PATH_RELATIVE?>index.php" target="_blank">here</a>.
+You can visit your new website <a href="http://<?php echo $_SERVER['HTTP_HOST'].PATH_RELATIVE?>index.php">here</a>.
 <br /><br />
