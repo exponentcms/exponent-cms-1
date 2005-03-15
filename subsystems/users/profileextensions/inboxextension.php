455831d102ce37c11fcf3a943103df423b4427f4
filetreefrog <>
2005-03-15 20:26:54 +0000

    Fixed a default value problem, so that the forwards checkbox is checked by default.

--- subsystems/users/profileextensions/inboxextension.php
+++ subsystems/users/profileextensions/inboxextension.php
@@ -36,15 +36,15 @@ class inboxextension {
 	function author() { return "James Hunt"; }
 	function description() { return "Allow users to configure email forwarding, and view their private messages from their profile."; }
 
-	function modifyForm($form,$user) { // new if !isset($user->id)
+	function modifyForm($form,$u) { // new if !isset($user->id)
 	
 		pathos_lang_loadDictionary('extras','inboxextension');
 	
-		if (!isset($user->_inbox_config)) {
-			$user->_inbox_config = inboxextension::_blank();
+		if (!isset($u->_inbox_config) || $u->_inbox_config == null) {
+			$u->_inbox_config = inboxextension::_blank();
 		}
 		$form->register(null,'',new htmlcontrol('<hr size="1" /><b>'.TR_X_INBOXEXTENSION_HEADER.'</b>'));
-		$form->register("inbox_forward",TR_X_INBOXEXTENSION_FORWARD, new checkboxcontrol($user->_inbox_config->forward,true));
+		$form->register("inbox_forward",TR_X_INBOXEXTENSION_FORWARD, new checkboxcontrol($u->_inbox_config->forward,true));
 		
 		return $form;
 	}
@@ -87,8 +87,9 @@ class inboxextension {
 	}
 	
 	function _blank() {
-		$cfg = null;;
+		$cfg = null;
 		$cfg->forward = 1;
+		return $cfg;
 	}
 }
 
