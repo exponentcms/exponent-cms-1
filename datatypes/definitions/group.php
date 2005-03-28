646da0d727962d9d86cc70f0cc3f9fa0e6ecf988
filetreefrog <>
2005-03-28 22:02:47 +0000

    Implemented RFE # 1112796, Group Signup Codes

--- datatypes/definitions/group.php
+++ datatypes/definitions/group.php
@@ -41,6 +41,9 @@ return array(
 	'name'=>array(
 		DB_FIELD_TYPE=>DB_DEF_STRING,
 		DB_FIELD_LEN=>200),
+	'code'=>array(
+		DB_FIELD_TYPE=>DB_DEF_STRING,
+		DB_FIELD_LEN=>200),
 	'description'=>array(
 		DB_FIELD_TYPE=>DB_DEF_STRING,
 		DB_FIELD_LEN=>1000),
