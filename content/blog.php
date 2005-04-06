d79557fb999f1baadc86c592c915c46c120a8107
filetreefrog <>
2005-04-06 17:01:18 +0000

    Added destinations for meaningful URLs

---
+++ content/blog.php
@@ -0,0 +1,10 @@
+<?php
+
+$_GET['action'] = 'view';
+$_GET['module'] = 'weblogmodule';
+$_REQUEST['action'] = 'view';
+$_REQUEST['module'] = 'weblogmodule';
+
+include_once('../index.php');
+
+?>
---
+++ content/news.php
@@ -0,0 +1,10 @@
+<?php
+
+$_GET['action'] = 'view';
+$_GET['module'] = 'newsmodule';
+$_REQUEST['action'] = 'view';
+$_REQUEST['module'] = 'newsmodule';
+
+include_once('../index.php');
+
+?>
