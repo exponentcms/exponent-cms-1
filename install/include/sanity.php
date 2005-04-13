b545362676dfe1d0776b7a489af15331a0fbcc62
jshinall_sf <>
2005-04-13 15:21:43 +0000

    Changed checkGD sanity check from '== 2.0' to '=== 2.0'
    --Jeremy

--- install/include/sanity.php
+++ install/include/sanity.php
@@ -159,7 +159,7 @@ function _sanity_checkGD() {
 	$info = gd_info();
 	if ($info['GD Version'] == 'Not Supported') {
 		return array(SANITY_WARNING,'Not Supported');
-	} else if (strpos($info['GD Version'],'2.0') == false) {
+	} else if (strpos($info['GD Version'],'2.0') === false) {
 		return array(SANITY_WARNING,'Older Version Installed ('.$info['GD Version'].')');
 	}
 	return array(SANITY_FINE,$info['GD Version']);
