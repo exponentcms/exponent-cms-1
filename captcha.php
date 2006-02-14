8a3b36e01a0eca1dcbb42b2746d5886cb7d46bcc
hanswolters <>
2006-02-14 09:13:16 +0000

    Typecasting image values for the captcha.

--- captcha.php
+++ captcha.php
@@ -24,6 +24,7 @@ $w = (isset($_GET['w']) ? $_GET['w'] : 200);
 $h = (isset($_GET['h']) ? $_GET['h'] : 50);
 $name = (isset($_GET['name']) ? $_GET['name'] : 'capcha_string');
 
+
 if (pathos_sessions_isset($name)) {
 	$str = pathos_sessions_get($name);
 } else {
@@ -31,7 +32,9 @@ if (pathos_sessions_isset($name)) {
 	pathos_sessions_set($name,$str);
 }
 
-$img = pathos_image_captcha($w,$h,$str);
+if (intval($w) > 0 && intval($h > 0))
+    $img = pathos_image_captcha($w,$h,$str);
+
 if ($img) {
 	$sizeinfo = array('mime'=>'image/png');
 	ob_end_clean();
