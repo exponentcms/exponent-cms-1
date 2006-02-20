f7ccdc4220130a66adcba7967d3cb03e28893e83
freddirkse <>
2006-02-20 14:50:00 +0000

    Bumped version to 96.5 Fred

--- pathos_version.php
+++ pathos_version.php
@@ -35,7 +35,7 @@ if (!defined('PATHOS_VERSION_MAJOR')) {
 	/* exdoc
 	 * This is the revision version number of Exponent; the 2 in 0.96.2-beta3
 	 */
-	define('PATHOS_VERSION_REVISION',4);
+	define('PATHOS_VERSION_REVISION',5);
 	/* exdoc
 	 * This is the date that this version of Exponent was exported from CVS and built.
 	 */
