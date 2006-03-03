ae242d67eb966462552672d6c1690170a5ae2537
jacobmesu <>
2006-03-03 13:04:47 +0000

    Bug 1434230 Fixed error: username can be void (added extra error string)

--- subsystems/lang/en/modules/loginmodule/actions/saveuser.php
+++ subsystems/lang/en/modules/loginmodule/actions/saveuser.php
@@ -4,6 +4,7 @@ return array(
 	'not_strong_enough'=>'Your password is not strong enough : %s',
 	'unmatched_passwords'=>'Passwords do not match.',
 	'username_taken'=>'That username is already taken.',
+	'username_too_short'=>'The username must be at least 3 characters long.',
 	'bad_captcha'=>'Verification failed.  Please re-enter the verification string (the image below)',
 );
 
