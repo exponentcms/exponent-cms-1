1794747280be151791a0218a5732798bb80b151d
freddirkse <>
2006-04-06 20:20:44 +0000

    Commented out debugging and refixed old bug.

--- modules/navigationmodule/actions/edit_contentpage.php
+++ modules/navigationmodule/actions/edit_contentpage.php
@@ -30,14 +30,14 @@ $check_id = -1;
 
 //echo serialize($fred);
 
-echo "<xmp>";
+//echo "<xmp>";
 //print_r($fred);
 //print_r($flim);
-print_r($user);
-echo "</xmp>";
+//print_r($user);
+//echo "</xmp>";
 
 
-if ($user->is_acting_admin == 1) {
+if ($user->is_acting_admin == 1 /*TODO: section admin*/) {
 	$section = null;
 	if (isset($_GET['id'])) {
 		// Check to see if an id was passed in get.  If so, retrieve that section from
@@ -47,33 +47,28 @@ if ($user->is_acting_admin == 1) {
 	} else if (isset($_GET['parent'])) {
 		// The isset check is merely a precaution.  This action should
 		// ALWAYS be invoked with a parent or id value in the GET.
-		$section->parent = intval($_GET['parent']);
+		//$section->parent = intval($_GET['parent']);
 		$check_id = $section->parent;
+		//$section->parent = $db->selectObject('section','parent='.intval($_GET['parent']));
 	}
-} else if (isset($_GET['parent'])) {
-	// The isset check is merely a precaution.  This action should
-	// ALWAYS be invoked with a parent or id value in the GET.
-	$section->parent = intval($_GET['parent']);
-	$check_id = $section->parent;
-}
 
-if ($check_id != -1 && exponent_permissions_check('manage',exponent_core_makeLocation('navigationmodule','',$check_id))) {	
-	$form = section::form($section);
-	$form->meta('module','navigationmodule');
-	$form->meta('action','save_contentpage');
-	// Create a template for the form output, to allow the themer to optionally
-	// change the form titles and captions, and to aide in translation.
-	$template = new template('navigationmodule','_form_editContentPage');
-	// Assign the concentional 'is_edit' flag to let the view show different text to the
-	// use in case of a create and an edit operation.
-	$template->assign('is_edit',isset($section->id));
-	// Assign the form/s rendered HTML to the template, using the customary
-	// name of 'form_html'
-	$template->assign('form_html',$form->toHTML());
-	$template->output();
-} else {
-	// User does not have permission to manage sections.  Throw a 403
-	echo SITE_403_HTML;
+	if (exponent_permissions_check('manage',exponent_core_makeLocation('navigationmodule','',$check_id))) {	
+		$form = section::form($section);
+		$form->meta('module','navigationmodule');
+		$form->meta('action','save_contentpage');
+		// Create a template for the form output, to allow the themer to optionally
+		// change the form titles and captions, and to aide in translation.
+		$template = new template('navigationmodule','_form_editContentPage');
+		// Assign the concentional 'is_edit' flag to let the view show different text to the
+		// use in case of a create and an edit operation.
+		$template->assign('is_edit',isset($section->id));
+		// Assign the form/s rendered HTML to the template, using the customary
+		// name of 'form_html'
+		$template->assign('form_html',$form->toHTML());
+		$template->output();
+	} else {
+		// User does not have permission to manage sections.  Throw a 403
+		echo SITE_403_HTML;
+	}
 }
-
 ?>
