45f052833dc0831d0c83e77b04c06ad430f68053
maxxcorp <>
2006-05-27 21:58:03 +0000

    temporary security fix, disabling the icon selector for mimetypes altogether - to be replaced by the yet to be written imagebrowser engine - hardcoded to use the binary.png icon

--- datatypes/mimetype.php
+++ datatypes/mimetype.php
@@ -17,6 +17,7 @@
 #
 ##################################################
 
+//TODO: bring back icon selector, this time based on the filebrowser engine
 class mimetype {
 	function form($object) {
 		$i18n = exponent_lang_loadFile('datatypes/mimetype.php');
@@ -30,36 +31,13 @@ class mimetype {
 			$form->meta('oldmime',$object->mimetype);
 		}
 		
-		
-		
 		$form->register('mimetype',$i18n['mimetype'], new textcontrol($object->mimetype));
 		$form->register('name',$i18n['name'],new textcontrol($object->name));
 		
 		$icodir = MIMEICON_RELATIVE;
 		$htmlimg = ($object->icon == '' ? '' : '<img src="'.MIMEICON_RELATIVE.$object->icon.'"/>');
 		// Replace this with something a little better.
-		$html = <<<EOD
-<span id="iconSPAN">$htmlimg</span><input type="hidden" id="iconHIDDEN" name="icon" value=""/>
-<script type="text/javascript">
-var g_span = document.getElementById("iconSPAN");
-var g_hidden = document.getElementById("iconHIDDEN");
 
-function setIcon(src) {
-	if (g_span.childNodes.length) {
-		g_span.removeChild(g_span.childNodes[0])
-	}
-	var img = g_span.appendChild(document.createElement("img"));
-	img.setAttribute("src",src);
-	
-	g_hidden.setAttribute("value",src);
-}
-</script>
-<a class="mngmntlink" href="" onClick="window.open('iconspopup.php?icodir=$icodir','icons','title=Icons,width=400,height=400'); return false">
-EOD;
-		$html .= $i18n['changeicon'];
-		$html .= '</a>';
-
-		$form->register('icon',$i18n['icon'], new customcontrol($html));
 		$form->register('submit','',new buttongroupcontrol($i18n['save'],'',$i18n['cancel']));
 		return $form;
 	}
@@ -67,7 +45,8 @@ EOD;
 	function update($values,$object) {
 		$object->mimetype = $values['mimetype'];
 		$object->name = $values['name'];
-		$object->icon = basename($_POST['icon']);
+		// temp fix
+		$object->icon = "binary.png";
 		
 		return $object;
 	}
