6781603a79d7ba73f0d385393236e027312310ba
maxxcorp <>
2006-06-19 14:49:53 +0000

    Ignacio Ibeas updated lang file

--- subsystems/lang/en/modules/calendarmodule/views/_view.php
+++ subsystems/lang/en/modules/calendarmodule/views/_view.php
@@ -1,14 +1,14 @@
 <?php
 
 return array(
-	'alt_userperm'=>'Assign user permissions on this Calendar Event',
-	'alt_groupperm'=>'Assign group permissions on this Calendar Event',
+	'alt_user_perms'=>'Assign user permissions on this Calendar Event',
+	'alt_group_perms'=>'Assign group permissions on this Calendar Event',
 	
 	'alt_edit'=>'Edit this Calendar Event',
 	'alt_edit_disabled'=>'Editting Disabled - Calendar Event In Approval',
 	
 	'alt_delete'=>'Delete this Calendar Event',
-	'delete_confirm'=>'Are you sure you want to delete this Calendar Event?',
+	'dlg_delete_confirm'=>'Are you sure you want to delete this Calendar Event?',
 	'alt_delete_disabled'=>'Deleting Disabled - Calendar Event In Approval',
 	'alt_revisions'=>'View Revision History for this Calendar Event',
 	'revisions'=>'Revisions',
