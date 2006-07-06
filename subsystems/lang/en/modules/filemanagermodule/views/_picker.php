d80cc32b3d88081f77511134c736353bb40ce8db
maxxcorp <>
2006-07-06 12:41:07 +0000

    some missing files from the i18n fixup by Robert Zelnik

---
+++ subsystems/lang/en/modules/filemanagermodule/views/_picker.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'form_title'=>'Testing Image Collections',
+
+	'uncategorized'=>'Uncategorized',
+	'no_files'=>'No files found.',
+	'img_upload'=>'Upload an Image:',
+);
+
+?>
