509a29bafb3172b9dfee86576ed17313601cd78e
maxxcorp <>
2006-08-28 16:43:15 +0000

    fix for bug #499 - insecure default password for admin user

--- install/pages/admin_user.php
+++ install/pages/admin_user.php
@@ -2,7 +2,7 @@
 
 ##################################################
 #
-# Copyright (c) 2004-2006 OIC Group, Inc.
+# Copyright (c) 2004-2006 OIC Group, Inc., Maxim Mueller
 # Written and Designed by James Hunt
 #
 # This file is part of Exponent
@@ -35,7 +35,57 @@ $i18n = exponent_lang_loadFile('install/pages/admin_user.php');
 	</div>
 	<div class="control">
 		&#0149; <span class="control_caption"><?php echo $i18n['password']; ?>: </span>
-		<input class="text" type="text" name="password" value="<?php echo $i18n['password_default']; ?>" />
+		<input class="text" type="text" name="password" value="
+<?php
+//		Written by Michael Bernd, adapted by Maxim Mueller for eXp
+//		Public Domain
+//
+//		letzte Änderung: Michael Berndt - Berlin
+//		Montag der 09. Mai. 2005
+//		08:01:53
+//
+// What is a secure password?
+//
+// This password has a minimum number of
+// - 8 characters
+// - at least 1 number
+// - at least 1 special character
+// - at least 1 mutation
+// - at least 1 small alphabetic character
+// - at least 1 capital alphabetic character
+
+function Berndt ($passwd_length){
+	$s[0]=array('0','1','2','3','4','5','6','7','8','9',);
+	$s[1]=array('!','"','§','%','/','(',')','=','?','`',
+			'#','+','*','~',';','.','-','|','<','>','^','°');
+	$s[2]=array('A','B','C','D','E','F','G','H','I','J','K',
+			'L','M','N','O','P','Q','R','S','T','U','V',
+			'W','X','Y','Z');
+	$s[3]=array('a','b','c','d','e','f','g','h','i','j','k',
+			'l','m','n','o','p','q','r','s','t','u','v',
+			'w','x','y','z');
+	$s[4]=array('Ä','Ö','Ü','ä','ö','ü','ß');
+
+	$l=count($s);
+
+	$a=array_merge($s[0],$s[1],$s[2],$s[3],$s[4]);
+	shuffle($a);
+	for($i=0;$i< $passwd_length;$i++){
+		if($i>=$l){
+			$p[]=$a[$i];
+		}else{
+			shuffle($s[$i]);
+			$p[]=$s[$i][0];
+		}
+	}
+	shuffle($p);
+	
+	return implode('',$p);
+}
+
+	echo Berndt(8);
+
+?>" />
 		<div class="control_help">
 			<?php echo $i18n['password_desc']; ?>
 		</div>
