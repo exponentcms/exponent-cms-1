2bcbb4c99dedddfa6150270e65df24c8fdae0683
kessler44 <>
2006-11-07 22:30:09 +0000

    *Added CodeMapModule *Added the article module to the base install - It was an add-on module *Added the Wizard Module to Exponent 	-required an update to administrationmodule/coretasks.php *Weblog module changes 	-Added in RSS support as default instead of addon patch 	-replaced missing i18n strings *Added two views to the textmodule.  The new views are "With Header" and "Without Header". *Added the Tasklist module. *Added the Slideshow Module. *Added the Simple Poll Module *Added a new action, View All, to the rotator module *Added the Page module to the base install - It was an add-on module *RSSified the newsmodule *Navigation Module views Added 	1. Children Only 2 	2. Children Only 3 	3. CSS Menu 	4. Full CSS Menu  	5. Site Map *Login Module changes 	-Added action "showuserprofile" *Added Listing Module to the base install - It was an add-on module *Added Link List Module *Added Imageworkshop module *Added Greeking Module *Form Module 	-Added form.validate.js (I don't know if this is new, moved or was accidently deleted from trunk) *Added FAQ module to the base install 	-Added the FAQ with table of contents view *Calendar Module 	-Added RSS 	-Added search action *Added Directory Listing Module *Added the new bbmodule *Added the imagegallery module along with the "View All" view and the "View All with Popups" *Added Smarty plugin for Currency Symbol *Container Module changes 	-Added infopopup functionality & view 	-New views 		1.  Two Column Homepage 		2.  home 		3.  rightcolumn *New user notifications 	-Added configuration to send email notification to site admin when new user signs up 	-Added configuration to send a welcome message to a new users after sign up. *Replaced default view for the search module with one that is more googlish.

---
+++ external/editors/FCKeditor_toolbox.js
@@ -0,0 +1,30 @@
+//##################################################
+//#
+//# Copyright (c) 2006 Maxim Mueller
+//#
+//# This file is part of Exponent
+//#
+//# Exponent is free software; you can redistribute
+//# it and/or modify it under the terms of the GNU
+//# General Public License as published by the Free
+//# Software Foundation; either version 2 of the
+//# License, or (at your option) any later version.
+//#
+//# GPL: http://www.gnu.org/licenses/gpl.txt
+//#
+//##################################################
+
+//this file provides an Array associating availiable Actions, their Icons, and, if required for this action, their plugins, with their internal ids
+//TODO: determine whether the Editor provides a queryable API for that
+//TODO: adjust for themes
+
+//TODO: possibly create a subobject WYSIWYG ?
+// first = action name
+// second = icon location
+// third = required plugin
+Exponent.WYSIWYG_toolboxbuttons =	{
+									"About" : ["About FCKEditor", "/external/editors/FCKeditor/editor/skins/default/toolbar/about.gif", ""],
+									"Image" : ["Image", "/external/editors/FCKeditor/editor/skins/default/toolbar/image.gif", ""],
+									"Link" : ["Link", "/external/editors/FCKeditor/editor/skins/default/toolbar/link.gif", ""],
+									"TableInsertRow" : ["Insert a Table", "/external/editors/FCKeditor/editor/skins/default/toolbar/tableinsertrow.gif", "tablecommands"]
+									};
