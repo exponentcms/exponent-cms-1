7478e5d467ec032b4cb6cb0cefb54c638b34fc58
kessler44 <>
2006-11-07 22:28:09 +0000

    *Added CodeMapModule *Added the article module to the base install - It was an add-on module *Added the Wizard Module to Exponent 	-required an update to administrationmodule/coretasks.php *Weblog module changes 	-Added in RSS support as default instead of addon patch 	-replaced missing i18n strings *Added two views to the textmodule.  The new views are "With Header" and "Without Header". *Added the Tasklist module. *Added the Slideshow Module. *Added the Simple Poll Module *Added a new action, View All, to the rotator module *Added the Page module to the base install - It was an add-on module *RSSified the newsmodule *Navigation Module views Added 	1. Children Only 2 	2. Children Only 3 	3. CSS Menu 	4. Full CSS Menu  	5. Site Map *Login Module changes 	-Added action "showuserprofile" *Added Listing Module to the base install - It was an add-on module *Added Link List Module *Added Imageworkshop module *Added Greeking Module *Form Module 	-Added form.validate.js (I don't know if this is new, moved or was accidently deleted from trunk) *Added FAQ module to the base install 	-Added the FAQ with table of contents view *Calendar Module 	-Added RSS 	-Added search action *Added Directory Listing Module *Added the new bbmodule *Added the imagegallery module along with the "View All" view and the "View All with Popups" *Added Smarty plugin for Currency Symbol *Container Module changes 	-Added infopopup functionality & view 	-New views 		1.  Two Column Homepage 		2.  home 		3.  rightcolumn *New user notifications 	-Added configuration to send email notification to site admin when new user signs up 	-Added configuration to send a welcome message to a new users after sign up. *Replaced default view for the search module with one that is more googlish.

---
+++ conf/extensions/registration.structure.php
@@ -0,0 +1,73 @@
+<?php
+
+##################################################
+#
+# Copyright (c) 2004-2006 OIC Group, Inc.
+# Copyright (c) 2006 Maxim Mueller
+# Written and Designed by James Hunt
+#
+# This file is part of Exponent
+#
+# Exponent is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License as published by the Free
+# Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# GPL: http://www.gnu.org/licenses/gpl.txt
+#
+##################################################
+
+if (!defined('EXPONENT')) exit('');
+
+$i18n = exponent_lang_loadFile('conf/extensions/registration.structure.php');
+
+$stuff = array(
+	$i18n['title'],
+	array(
+		'SITE_ALLOW_REGISTRATION'=>array(
+			'title'=>$i18n['allow_registration'],
+			'description'=>$i18n['allow_registration_desc'],
+			'control'=>new checkboxcontrol()
+		),
+		'SITE_USE_CAPTCHA'=>array(
+			'title'=>$i18n['use_captcha'],
+			'description'=>$i18n['use_captcha_desc'],
+			'control'=>new checkboxcontrol()
+		),
+		'USER_REGISTRATION_SEND_NOTIF'=>array(
+			'title'=>$i18n['user_registration_send_notif'],
+			'description'=>$i18n['user_registration_send_notif_desc'],
+			'control'=>new checkboxcontrol()
+		),
+		'USER_REGISTRATION_NOTIF_SUBJECT'=>array(
+			'title'=>$i18n['user_registration_notif_subject'],
+			'description'=>$i18n['user_registration_notif_subject_desc'],
+			'control'=>new textcontrol('',50)
+		),
+		'USER_REGISTRATION_ADMIN_EMAIL'=>array(
+			'title'=>$i18n['user_registration_admin_email'],
+			'description'=>$i18n['user_registration_admin_email_desc'],
+			'control'=>new textcontrol('',50)
+		),
+		'USER_REGISTRATION_SEND_WELCOME'=>array(
+			'title'=>$i18n['user_registration_send_welcome'],
+			'description'=>$i18n['user_registration_send_welcome_desc'],
+			'control'=>new checkboxcontrol()
+		),
+		'USER_REGISTRATION_WELCOME_SUBJECT'=>array(
+			'title'=>$i18n['user_registration_welcome_subject'],
+			'description'=>$i18n['user_registration_welcome_subject_desc'],
+			'control'=>new textcontrol('',50)
+		),
+		'USER_REGISTRATION_WELCOME_MSG'=>array(
+			'title'=>$i18n['user_registration_welcome_msg'],
+			'description'=>$i18n['user_registration_welcome_msg_desc'],
+			'control'=>new texteditorcontrol('',15,50)
+		),
+	)
+);
+
+return $stuff;
+
+?>
