55e3c8ba0a12907bf7262e8ae9fc06a5d288edb8
kessler44 <>
2006-12-21 23:41:06 +0000

    Added text for the setlang variable

--- subsystems/lang/eng_US/install/index.php
+++ subsystems/lang/eng_US/install/index.php
@@ -1,6 +1,7 @@
 <?php
 
 return array(
+	'setlang'=>'<br>Please choose which language you would like to use for this install.',
 	'sanity'=>'Exponent requires that several permissions be set correctly in order to operate.  Sanity checks are being run right now to ensure that the web server directory you wish to install Exponent in is suitable.<br /><br />If something fails, please <a href="#" onClick="return pop(\'sanity\');">read about each sanity check</a> for an explanation of what exactly the installer is checking for, and how to fix it.',
 	'dbconfig'=>'In order to best manage your content, Exponent stores it in a relational database.  Configuring your database connection properly is the most important step in setting up your new website.<br /><br />Due to security reasons, you or your web server administrator will have to create an empty database.  For information on how to create the database, click <a href="" onClick="return pop(\'db_create\');">here</a>.  Optionally, you may choose to use a pre-existing database.  For information on using an existing database and what it entails, click <a href="" onClick="return pop(\'db_existing\');">here</a>.',
 	'dbcheck'=>'The Exponent Install Wizard will now check to make sure that the database configuration information you provided is valid.',
