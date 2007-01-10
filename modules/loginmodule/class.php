85a273d2cdd7206693267c50a42f40ba93724b2b
kessler44 <>
2007-01-10 20:43:33 +0000

    Changed the author to OIC Group

--- modules/loginmodule/class.php
+++ modules/loginmodule/class.php
@@ -19,7 +19,7 @@
 
 class loginmodule {
 	function name() { return exponent_lang_loadKey('modules/loginmodule/class.php','module_name'); }
-	function author() { return 'James Hunt'; } 
+	function author() { return 'OIC Group, Inc'; } 
 	function description() { return exponent_lang_loadKey('modules/loginmodule/class.php','module_description'); }
 	
 	function hasContent() { return false; }
