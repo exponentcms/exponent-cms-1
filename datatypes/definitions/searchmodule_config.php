83f1371b7c0daa9768650d3a4ff23b5c573c85d9
kessler44 <>
2007-01-26 20:24:29 +0000

    Added the ability to select which modules to search in the search module.

--- datatypes/definitions/searchmodule_config.php
+++ datatypes/definitions/searchmodule_config.php
@@ -30,7 +30,10 @@ return array(
 		DB_FIELD_LEN=>200,
                 DB_INDEX=>10),
 	'is_categorized'=>array(
-		DB_FIELD_TYPE=>DB_DEF_BOOLEAN)
+		DB_FIELD_TYPE=>DB_DEF_BOOLEAN),
+	'modules'=>array(
+                DB_FIELD_TYPE=>DB_DEF_STRING,
+                DB_FIELD_LEN=>10000),
 );
 
 ?>
--- datatypes/searchmodule_config.php
+++ datatypes/searchmodule_config.php
@@ -22,16 +22,53 @@ class searchmodule_config {
 		$i18n = exponent_lang_loadFile('datatypes/searchmodule_config.php');
 	
 		if (!defined('SYS_FORMS')) require_once(BASE.'subsystems/forms.php');
+		if (!defined('SYS_SEARCH')) require_once(BASE.'subsystems/search.php');
 		exponent_forms_initialize();
 		
 		$form = new form();
 		if (!isset($object->id)) {
 			$object->is_categorized = 0;
+			$modules = null;
 		} else {
 			$form->meta('id',$object->id);
+			$modules = unserialize($object->modules);
 		}
 		
 		$form->register('is_categorized',$i18n['is_categorized'],new checkboxcontrol($object->is_categorized,true));
+		
+		$form->register(null,'',new htmlcontrol('<br><br><div class="moduletitle">Searchable Modules</div><hr size="1" />'));	
+		$form->register(null,'',new htmlcontrol('Select which parts of your site will be searchable by users using this search module.'));	
+		$form->register(null,'',new htmlcontrol('<hr size="1" />'));	
+		//eDebug(exponent_modules_list()); exit();
+		
+		$mod_list = getModuleNames(null);
+		$selected_mods = getModuleNames($modules);
+		
+		$td_count = 0;
+		$html = '<table cellpadding="0" cellspacing="0" border="0" class="search_modulelist"><tr>';
+		foreach ($mod_list as $key => $mod_name) {
+			if ($td_count == 3) {
+				$html .= '</tr><tr><td class="search_modulelist">';
+				$td_count = 1;
+			} else {
+				$html .= '<td class="search_modulelist">';
+				$td_count += 1;
+			}
+		
+			if (array_key_exists($key, $selected_mods)) { $selected="checked"; } else { $selected=''; }		
+			$html .= '<input type="checkbox" name="modules[]" value="'.$key.'"'.$selected.'> &nbsp;&nbsp;'.$mod_name.'</td>';
+			
+		}
+		
+		if ($td_count != 3) {
+			for ($i=$td_count; $i < 3; $i++) {
+				$html .= '<td>&nbsp;</td>';
+			}
+		}
+
+		$html .= '</tr></table>';
+		$form->register(null,'',new htmlcontrol($html));	
+
 		$form->register('submit','',new buttongroupcontrol($i18n['save'],'',$i18n['cancel']));
 		
 		return $form;
@@ -39,8 +76,10 @@ class searchmodule_config {
 	
 	function update($values,$object) {
 		$object->is_categorized = (isset($values['is_categorized']) ? 1 : 0);
+		$object->modules = serialize($values['modules']);
 		return $object;
 	}
+
 }
 
 ?>
