0e013f3c1fca28f6e2e05d989a3c4b15045aa5d0
kessler44 <>
2007-02-02 17:26:50 +0000

    Making FCK the default editor

--- conf/extensions/site.defaults.php
+++ conf/extensions/site.defaults.php
@@ -30,7 +30,7 @@ if (!defined('SITE_KEYWORDS')) define('SITE_KEYWORDS','');
 if (!defined('SITE_DESCRIPTION')) define('SITE_DESCRIPTION','');
 if (!defined('SITE_DEFAULT_SECTION')) define('SITE_DEFAULT_SECTION',1);
 
-if (!defined('SITE_WYSIWYG_EDITOR')) define('SITE_WYSIWYG_EDITOR','Xinha');
+if (!defined('SITE_WYSIWYG_EDITOR')) define('SITE_WYSIWYG_EDITOR','FCKeditor');
 
 if (!defined('SESSION_TIMEOUT')) define('SESSION_TIMEOUT',3600*5);
 if (!defined('SESSION_TIMEOUT_HTML')) define('SESSION_TIMEOUT_HTML',exponent_unhtmlentities('<h3>Expired Login Session</h3>Your session has expired, because you were idle too long.  You will have to log back into the system to continue what you were doing.'));
