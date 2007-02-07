707e257b5a4fd9d5a5f5de29af431d50e4546ff4
kessler44 <>
2007-02-07 17:48:16 +0000

    Changed the author function

--- modules/textmodule/class.php
+++ modules/textmodule/class.php
@@ -19,7 +19,7 @@
 
 class textmodule {
 	function name()		{ return exponent_lang_loadKey('modules/textmodule/class.php','module_name'); }
-	function author()		{ return 'James Hunt'; }
+	function author()		{ return 'OIC Group, Inc'; }
 	function description()	{ return exponent_lang_loadKey('modules/textmodule/class.php','module_description'); }
 
 	function hasContent() { return true; }
