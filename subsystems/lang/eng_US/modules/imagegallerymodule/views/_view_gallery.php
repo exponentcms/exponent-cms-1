d0a3ce8538a7fa7e3ddcbd40c731e2816c23cd93
maia_good <>
2007-02-25 21:24:50 +0000

    Add translation strings for module / view.

---
+++ subsystems/lang/eng_US/modules/imagegallerymodule/views/_view_gallery.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+	'page_of'=>'Page %d of %d',
+	'image_plural'=>'images',
+	'image_singular'=>'image',
+	'image_in_gallery'=>'%s in the gallery.',
+	'by_name'=>'By Name',
+	'by_creation_date'=>'By Date of Creation',
+	'ascending'=>'Ascending',
+	'descending'=>'Descending',
+	'sort_images'=>'Order Images',
+	'previous'=>'Previous',
+	'next'=>'Next',
+	'upload_files'=>'Upload images:',
+	'upload'=>'Upload',
+	'whole_number'=>'Please enter only positive, whole numbers.'
+
+);
+
+?>
