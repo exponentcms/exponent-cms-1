f5f57a5425b6b6aef83f25b3d8c458a9d46c70c4
kessler44 <>
2007-05-19 02:40:27 +0000

    Changed the login.php file to just call the loginredirect action in the theme_main.

--- login.php
+++ login.php
@@ -17,37 +17,11 @@
 #
 ##################################################
 
-define('SCRIPT_EXP_RELATIVE','');
-define('SCRIPT_FILENAME','login.php');
-
-ob_start();
-
 // Initialize the Exponent Framework
 require_once('exponent.php');
 
-// Initialize the Sessions Subsystem
-if (!defined('SYS_SESSIONS')) require_once(BASE.'subsystems/sessions.php');
-// Initialize the Theme Subsystem
-if (!defined('SYS_THEME')) require_once(BASE.'subsystems/theme.php');
+//Fire off the login form via an exponent action.
+header("Location: ".URL_FULL."?module=loginmodule&action=loginredirect");
 
-if (exponent_sessions_loggedIn()) {
-	$SYS_FLOW_REDIRECTIONPATH = 'exponent_default';
-	exponent_flow_redirect();
-	exit('Redirecting...');
-} else if (isset($_REQUEST['module']) && isset($_REQUEST['action'])) {
-	$SYS_FLOW_REDIRECTIONPATH = 'loginredirect'; 
-	exponent_theme_runAction();
-	loginmodule::show(DEFAULT_VIEW,null);
-} else {
-	$SYS_FLOW_REDIRECTIONPATH = 'loginredirect'; 
-	exponent_flow_set(SYS_FLOW_PUBLIC,SYS_FLOW_SECTIONAL);
-	loginmodule::show(DEFAULT_VIEW,null);
-}
-
-$template = new standalonetemplate('loginredirect');
-
-$template->assign('output',ob_get_contents());
-ob_end_clean();
-$template->output();
 
 ?>
