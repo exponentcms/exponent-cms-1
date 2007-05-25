cfbf5c4ed4f91f0f5861d2b965908df1f06c8832
freddirkse <>
2007-05-25 15:31:41 +0000

    Added upgrade from 0.96.6 - All Versions

--- install/pages/upgrade.php
+++ install/pages/upgrade.php
@@ -22,7 +22,49 @@ if (!defined('EXPONENT')) exit('');
 
 $errors = array();
 
-include_once(BASE . 'modules/administrationmodule/actions/installtables.php');
+//include_once(BASE . 'modules/administrationmodule/actions/installtables.php');
+
+/* This code is copied from installtables.php in the admin module, which has permissions.
+* Be sure to copy any changes there if modifying this code */
+	
+	define("TMP_TABLE_EXISTED",		1);
+	define("TMP_TABLE_INSTALLED",	2);
+	define("TMP_TABLE_FAILED",		3);
+	define("TMP_TABLE_ALTERED",		4);
+	
+	$dir = BASE."datatypes/definitions";
+	if (is_readable($dir)) {
+		$tables = array();
+		$dh = opendir($dir);
+		while (($file = readdir($dh)) !== false) {
+			if (is_readable("$dir/$file") && is_file("$dir/$file") && substr($file,-4,4) == ".php" && substr($file,-9,9) != ".info.php") {
+				$tablename = substr($file,0,-4);
+				$dd = include("$dir/$file");
+				$info = null;
+				if (is_readable("$dir/$tablename.info.php")) $info = include("$dir/$tablename.info.php");
+				if (!$db->tableExists($tablename)) {
+					foreach ($db->createTable($tablename,$dd,$info) as $key=>$status) {
+						$tables[$key] = $status;
+					}
+				} else {
+					foreach ($db->alterTable($tablename,$dd,$info) as $key=>$status) {
+						if ($status == TABLE_ALTER_FAILED){
+							$tables[$key] = $status;
+						}else{
+							$tables[$key] = ($status == TABLE_ALTER_NOT_NEEDED ? DATABASE_TABLE_EXISTED : DATABASE_TABLE_ALTERED);						
+						}
+						
+					}
+				}
+			} 
+		}
+		ksort($tables);
+		
+		$template = new template("administrationmodule","_tableInstallSummary",$loc);
+		$template->assign("status",$tables);	
+		$template->output();
+	}
+//end copy from installtable.php
 
 //Run the upgrade scripts
 $upgrade_dir = 'upgrades/'.$_POST['from_version'];
--- install/pages/upgrade_version.php
+++ install/pages/upgrade_version.php
@@ -22,6 +22,7 @@ if (!defined('EXPONENT')) exit('');
 $versions = array(	
 	'0.96.3'=>'Origin',	
 	'0.96.5'=>'All Versions',	
+	'0.96.6'=>'All Versions',	
 );
 
 $i18n = exponent_lang_loadFile('install/pages/upgrade_version.php');
