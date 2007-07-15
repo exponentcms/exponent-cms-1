2c61a63286978b40a33e11a57f4a0caa5fe78c4d
maia_good <>
2007-07-15 11:33:50 +0000

    Add language files to module.

---
+++ subsystems/lang/eng_US/modules/faqmodule/views/Default.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+	'default_view'=>'Default View',
+	'toggle_view'=>'Toggle Answer View',
+
+	'alt_userperm'=>'Assign user permissions on this FAQ Module',
+	'alt_groupperm'=>'Assign group permissions on this FAQ Module',
+
+	'alt_configure'=>'Configure this FAQ Module',
+
+	'manage_categories'=>'Manage Categories',
+
+	'no_category'=>'Not Categorized',
+
+	'view_categories'=>'View Categories',
+	'alt_view_cat'=>'View FAQ Categories',
+
+
+);
+
+?>
