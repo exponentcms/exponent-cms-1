c1f3fe3d9928b84f4ca2d41b1cd36773e8923fb2
maia_good <>
2007-07-15 11:34:38 +0000

    Add language files to module.

---
+++ subsystems/lang/eng_US/modules/articlemodule/views/Default.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+	'default_view'=>'Default View',
+	'toggle_view'=>'Toggle Answer View',
+
+	'alt_userperm'=>'Assign user permissions on this Article Module',
+	'alt_groupperm'=>'Assign group permissions on this Article Module',
+
+	'alt_configure'=>'Configure this FAQ Module',
+
+	'manage_categories'=>'Manage Categories',
+
+	'no_category'=>'Not Categorized',
+
+	'view_categories'=>'View Categories',
+	'alt_view_cat'=>'View Article Categories',
+
+	'delete_confirm'=>'Are you sure you want to delete this article?',
+	'new_entry'=>'New Article',
+
+
+);
+
+?>
