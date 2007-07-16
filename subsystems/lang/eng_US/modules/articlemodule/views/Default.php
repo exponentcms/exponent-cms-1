e12567443cdd4cba81f44531ffac5e27c9a13e52
maia_good <>
2007-07-16 05:47:38 +0000

    Complete adding string translations.

--- subsystems/lang/eng_US/modules/articlemodule/views/Default.php
+++ subsystems/lang/eng_US/modules/articlemodule/views/Default.php
@@ -7,17 +7,25 @@ return array(
 	'alt_userperm'=>'Assign user permissions on this Article Module',
 	'alt_groupperm'=>'Assign group permissions on this Article Module',
 
-	'alt_configure'=>'Configure this FAQ Module',
+	'alt_configure'=>'Configure this Article Module',
 
-	'manage_categories'=>'Manage Categories',
+	'alt_edit'=>'Edit this entry',
+	'alt_delete'=>'Delete this entry',
+	'alt_up'=>'Move entry up',
+	'alt_up_disabled'=>'Move up disabled',
+	'alt_down'=>'Move entry down',
+	'alt_down_disabled'=>'Move down disabled',
 
 	'no_category'=>'Not Categorized',
 
 	'view_categories'=>'View Categories',
 	'alt_view_cat'=>'View Article Categories',
+	'manage_categories'=>'Manage Categories',
 
-	'delete_confirm'=>'Are you sure you want to delete this article?',
+	'delete_confirm'=>'Are you sure you want to delete this Article?',
 	'new_entry'=>'New Article',
+
+	'no_article'=>'No articles were found.',
 
 
 );
