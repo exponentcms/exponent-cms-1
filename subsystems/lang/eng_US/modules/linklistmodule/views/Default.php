1029538385f70ef872577e93ef903f54f5e5a36d
kessler44 <>
2007-09-19 04:53:15 +0000

    Adding some i18n files for the linklist module.

---
+++ subsystems/lang/eng_US/modules/linklistmodule/views/Default.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	'no_link'=>'No links have been entered yet.',
+	'new_link'=>'Add a new link',
+	'delete_link_confirm'=>'Are you sure you want to delete this link?',
+);
+
+?>
