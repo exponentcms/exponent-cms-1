e1bd24e95ed4561ed4c591e930b1528f8b374e8f
optiondelete <>
2007-11-20 15:59:29 +0000

    changes

--- subsystems/forms/controls/yuieditorcontrol.php
+++ subsystems/forms/controls/yuieditorcontrol.php
@@ -84,7 +84,42 @@ class yuieditorcontrol extends formcontrol {
 		    animate: true, //Animates the opening, closing and moving of Editor windows
 			handleSubmit:true
 		});
-		myEditor.render();		
+		//Wait for the editor's toolbar to load
+		myEditor.on('toolbarLoaded', function() {
+		    //create the new gutter object
+		    gutter = new YAHOO.gutter();
+
+		    //The Toolbar buttons config
+		    var flickrConfig = {
+		            type: 'push',
+		            label: 'Insert Flickr Image',
+		            value: 'flickr'
+		    }
+
+		    //Add the button to the insertitem group
+		    myEditor.toolbar.addButtonToGroup(flickrConfig, 'insertitem');
+
+		    //Handle the button's click
+		    myEditor.toolbar.on('flickrClick', function() {
+		        this._focusWindow();
+		        if (ev && ev.img) {
+		            //To abide by the Flickr TOS, we need to link back to the image that we just inserted
+		            var html = '<a href=\"' + ev.url + '\"><img src=\"' + ev.img + '\" title=\"' + ev.title + '\"></a>';
+		            this.execCommand('inserthtml', html);
+		        }
+		        //Toggle the gutter, so that it opens and closes based on this click.
+		        gutter.toggle();
+		    });
+		    //Create the gutter control
+		    gutter.createGutter();
+		});
+		myEditor.render();
+		
+		
+		
+		
+		
+			
 		</script>
 		";
 		return $html;
