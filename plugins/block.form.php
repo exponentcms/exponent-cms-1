32e5c4efcd4ba8bb657571079a4eb5d4058d9e23
kessler44 <>
2007-11-27 22:52:30 +0000

    There was a bug in this plugin where an extra slash was being added to the paths of the javascript files being included which caused them to fail to include.  Fixed this.

--- plugins/block.form.php
+++ plugins/block.form.php
@@ -18,33 +18,35 @@
 ##################################################
 
 function smarty_block_form($params,$content,&$smarty, &$repeat) {
-	if(!$content){
-	//handle incoming errors
-	
-	if (exponent_sessions_isset("last_POST")) {
-		$formError  = exponent_sessions_get("last_POST");
-		$smarty->assign($params['last_POST'],$formError);
-		exponent_sessions_unset("last_POST");
-	}
+	if(empty($content)){
+		//handle incoming errors
+		
+		if (exponent_sessions_isset("last_POST")) {
+			$formError  = exponent_sessions_get("last_POST");
+			$smarty->assign($params['last_POST'],$formError);
+			exponent_sessions_unset("last_POST");
+		}
 		
 		
-	$name = isset($params['name']) ? $params['name'] : 'form';
-	echo "<!-- Form Object 'form' -->\r\n";
-	echo '<script type="text/javascript" src="'.PATH_RELATIVE.'subsystems/forms/js/inputfilters.js.php"></script>'."\r\n";
-	echo '<script type="text/javascript" src="'.PATH_RELATIVE.'subsystems/forms/controls/listbuildercontrol.js"></script>'."\r\n";
-	echo '<script type="text/javascript" src="'.PATH_RELATIVE.'subsystems/forms/js/required.js"></script>'."\r\n";
-	echo '<script type="text/javascript" src="'.PATH_RELATIVE.'external/jscalendar/calendar.js"></script>'."\r\n";
-	echo '<script type="text/javascript" src="'.PATH_RELATIVE.'external/jscalendar/lang/calendar-en.js"></script>'."\r\n";
-	echo '<script type="text/javascript" src="'.PATH_RELATIVE.'external/jscalendar/calendar-setup.js"></script>'."\r\n";
-	echo '<script type="text/javascript" src="'.PATH_RELATIVE.'js/PopupDateTimeControl.js"></script>'."\r\n";
-	echo '<form id="'.$name.'" name="'.$name.'" method="post" action="index.php" enctype="'.$params['enctype'].'">'."\r\n";
-	echo '<input type="hidden" name="module" id="module" value="'.$smarty->_tpl_vars['__loc']->mod.'" />'."\r\n";
-	echo '<input type="hidden" name="src" id="src" value="'.$smarty->_tpl_vars['__loc']->src.'" />'."\r\n";
-	echo '<input type="hidden" name="int" id="int" value="'.$smarty->_tpl_vars['__loc']->int.'" />'."\r\n";
-	echo isset ($params['ajax']) ? '<input type="hidden" name="ajax_action" value="1" />'."\r\n" : '';
-	if (isset($params['action']))  echo '<input type="hidden" name="action" id="action" value="'.$params['action'].'" />'."\r\n";
+		$name = isset($params['name']) ? $params['name'] : 'form';
+		$module = isset($params['module']) ? $params['module'] : $smarty->_tpl_vars['__loc']->mod;
+
+		echo "<!-- Form Object 'form' -->\r\n";
+		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'subsystems/forms/js/inputfilters.js.php"></script>'."\r\n";
+		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'subsystems/forms/controls/listbuildercontrol.js"></script>'."\r\n";
+		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'subsystems/forms/js/required.js"></script>'."\r\n";
+		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'external/jscalendar/calendar.js"></script>'."\r\n";
+		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'external/jscalendar/lang/calendar-en.js"></script>'."\r\n";
+		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'external/jscalendar/calendar-setup.js"></script>'."\r\n";
+		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'js/PopupDateTimeControl.js"></script>'."\r\n";
+		echo '<form id="'.$name.'" name="'.$name.'" method="post" action="index.php" enctype="'.$params['enctype'].'">'."\r\n";
+		echo '<input type="hidden" name="module" id="module" value="'.$module.'" />'."\r\n";
+		echo '<input type="hidden" name="src" id="src" value="'.$smarty->_tpl_vars['__loc']->src.'" />'."\r\n";
+		echo '<input type="hidden" name="int" id="int" value="'.$smarty->_tpl_vars['__loc']->int.'" />'."\r\n";
+		echo isset ($params['ajax']) ? '<input type="hidden" name="ajax_action" value="1" />'."\r\n" : '';
+		if (isset($params['action']))  echo '<input type="hidden" name="action" id="action" value="'.$params['action'].'" />'."\r\n";
 	
-	//echo the innards
+		//echo the innards
 	}else{	
 		echo $content;	
 		echo '</form>';
