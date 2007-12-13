96d433f0fc8b7c1848f1100e072e6e24ad18ea7c
maxxcorp <>
2007-12-13 00:29:12 +0000

    magic quotes have been removed in php6, make the call to that function conditional

--- exponent_bootstrap.php
+++ exponent_bootstrap.php
@@ -18,15 +18,18 @@
 ##################################################
 
 # Following code taken from http://us4.php.net/manual/en/function.get-magic-quotes-gpc.php
-#   - it allows magic_quotes to be on without screwing stuff up. 
-if (get_magic_quotes_gpc()) {
-	function stripslashes_deep($value) {
-		return is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value);
-	}
+#   - it allows magic_quotes to be on without screwing stuff up.
+# magic quotes were removed in php6
+if(phpversion() < 6) { 
+	if (get_magic_quotes_gpc()) {
+		function stripslashes_deep($value) {
+			return is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value);
+		}
 
-	$_POST = stripslashes_deep($_POST);
-	$_GET = stripslashes_deep($_GET);
-	$_COOKIE = stripslashes_deep($_COOKIE);
+		$_POST = stripslashes_deep($_POST);
+		$_GET = stripslashes_deep($_GET);
+		$_COOKIE = stripslashes_deep($_COOKIE);
+	}
 }
 
 // exponent.php (the file that includes this file the most) will define this for its own purposes
