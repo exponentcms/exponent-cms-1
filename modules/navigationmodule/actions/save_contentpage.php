bf59dec204b088cdd863c6d5ca1c144fd92a04ce
kessler44 <>
2008-01-23 19:43:40 +0000

    fixed a bug in the new code that creates an SEF name when the use doesn't specify one

--- modules/navigationmodule/actions/save_contentpage.php
+++ modules/navigationmodule/actions/save_contentpage.php
@@ -47,20 +47,10 @@ if (($check_id != -1 &&
 	)) {
 
 	// make sure the SEF name is valid
-	if (!empty($section->sef_name)) {
-		if (!section::isValidName($section->sef_name)) validator::failAndReturnToForm('You have invalid characters in the SEF Name field.');
-		if (section::isDuplicateName($section)) validator::failAndReturnToForm('The name specified in the SEF Name field is a duplicate of an existing page.');	
-	} else {
-		if (!section::isValidName($section->name) && empty($section->sef_name)) validator::failAndReturnToForm('You have invalid characters in the Name field.');
-
-		global $router;
-		$section->sef_name = $router->encode($section->name);
-		if (section::isDuplicateName($section)) validator::failAndReturnToForm('The name specified in the SEF Name field is a duplicate of an existing page.');
-	}
-
-	//check the name field
-	//if (!section::isValidName($section->name) && empty($section->sef_name)) validator::failAndReturnToForm('You have invalid characters in the Name field.');
-	//if (section::isDuplicateName($section->name)) validator::failAndReturnToForm('The name specified in the Name field is a duplicate of an existing page.');	
+        global $router;
+        if (empty($section->sef_name)) $section->sef_name = $router->encode($section->name);
+        if (!section::isValidName($section->sef_name)) validator::failAndReturnToForm('You have invalid characters in the SEF Name field.');
+        if (section::isDuplicateName($section)) validator::failAndReturnToForm('The name specified in the SEF Name field is a duplicate of an existing page.');
 
 	if (isset($section->id)) {
 		if ($section->parent != $old_parent) {
