d056d2c3da47ef6f477f033b5932f387023cecb9
optiondelete <>
2008-01-28 21:26:58 +0000

    set coolwater theme as default

--- conf/extensions/display.defaults.php
+++ conf/extensions/display.defaults.php
@@ -18,7 +18,7 @@
 ##################################################
 
 if (!defined('DISPLAY_LANGUAGE')) define('DISPLAY_LANGUAGE','en');
-if (!defined('DISPLAY_THEME_REAL')) define('DISPLAY_THEME_REAL','basetheme');
+if (!defined('DISPLAY_THEME_REAL')) define('DISPLAY_THEME_REAL','coolwatertheme');
 if (!defined('DISPLAY_ATTRIBUTION')) define('DISPLAY_ATTRIBUTION','username');
 if (!defined('DISPLAY_DATETIME_FORMAT')) define('DISPLAY_DATETIME_FORMAT','%D -- %T');
 if (!defined('DISPLAY_DATE_FORMAT')) define('DISPLAY_DATE_FORMAT','%D');
