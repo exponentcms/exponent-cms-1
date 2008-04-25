20c2de5c4703894642c3e2c0474a0ea2cb8a26a9
kessler44 <>
2008-04-25 18:57:53 +0000

    removing

--- themes/common/javascript/containermodule/Default.js
+++
@@ -1,5 +0,0 @@
-var contextElements = YAHOO.util.Dom.getElementsByClassName("viewinfo");
-
-for (var i=0;i<=contextElements.length;i++) {
-	var tooltip = new YAHOO.widget.Tooltip("tt"+i, { context:contextElements[i] });
-}
