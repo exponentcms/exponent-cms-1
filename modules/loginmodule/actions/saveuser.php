0a325dd685a5dd1ffe6920e9b747b6cb45f36daf
optiondelete <>
2008-05-21 16:44:10 +0000

    BUG FIX: Updated with Validator class to correctly report signup errors

--- modules/loginmodule/actions/saveuser.php
+++ modules/loginmodule/actions/saveuser.php
@@ -27,49 +27,30 @@ if ( (!$user || $user->id==0) && SITE_ALLOW_REGISTRATION == 1) {
 	if (!defined('SYS_SECURITY')) require_once(BASE.'subsystems/security.php');
 	$username_error = exponent_security_checkUsername($_POST['username']);
 	if ($username_error != '')	{
-		$post = $_POST;
-		unset($post['username']);		
-		$post['_formError'] = sprintf($i18n['username_failed'],$username_error);
-		exponent_sessions_set('last_POST',$post);
-		header('Location: ' . $_SERVER['HTTP_REFERER']);
+		unset($_POST['username']);		
+		validator::failAndReturnToForm(sprintf($i18n['username_failed'],$username_error), $_POST);
 	}
 	else if (exponent_users_getUserByName($_POST['username']) != null) {
-		$post = $_POST;
-		unset($post['username']);
-		$post['_formError'] = $i18n['username_taken'];
-		exponent_sessions_set('last_POST',$post);
-		header('Location: ' . $_SERVER['HTTP_REFERER']);
+		unset($_POST['username']);
+		validator::failAndReturnToForm($i18n['username_taken'], $_POST);
 	} else if ($_POST['pass1'] != $_POST['pass2']) {
-		$post = $_POST;
-		unset($post['pass1']);
-		unset($post['pass2']);
-		$post['_formError'] = $i18n['unmatched_passwords'];
-		exponent_sessions_set('last_POST',$post);
-		header('Location: ' . $_SERVER['HTTP_REFERER']);
+		unset($_POST['pass1']);
+		unset($_POST['pass2']);
+		validator::failAndReturnToForm($i18n['unmatched_passwords'], $_POST);
 	} else {		
 		$strength_error = exponent_security_checkPasswordStrength($_POST['username'],$_POST['pass1']);
 		if ($strength_error != '') {
-			$post = $_POST;
-			unset($post['pass1']);
-			unset($post['pass2']);
-			$post['_formError'] = sprintf($i18n['not_strong_enough'],$strength_error);
-			exponent_sessions_set('last_POST',$post);
-			header('Location: ' . $_SERVER['HTTP_REFERER']);
+			unset($_POST['pass1']);
+			unset($_POST['pass2']);
+			validator::failAndReturnToForm(sprintf($i18n['not_strong_enough'],$strength_error), $_POST);
 		} else {
 			// Finally, check the capcha
-			if (SITE_USE_CAPTCHA && strtoupper($_POST['captcha_string']) != $capcha_real) {
-				$post = $_POST;
-				unset($post['captcha_string']);
-				$post['_formError'] = $i18n['bad_captcha'];
-				exponent_sessions_set('last_POST',$post);
-				header('Location: ' . $_SERVER['HTTP_REFERER']);
-			} else {
-				exponent_sessions_unset('capcha_string');
-				$u = exponent_users_create($_POST,null);
-				$u = exponent_users_saveProfileExtensions($_POST,$u,true);
-				exponent_users_login($_POST['username'],$_POST['pass1']);
-				exponent_flow_redirect();
-			}
+			validator::validate(array('captcha'=>'captcha_string'), $_POST);
+			exponent_sessions_unset('capcha_string');
+			$u = exponent_users_create($_POST,null);
+			$u = exponent_users_saveProfileExtensions($_POST,$u,true);
+			exponent_users_login($_POST['username'],$_POST['pass1']);
+			exponent_flow_redirect();
 		}
 	}
 } else {
