ed44f080e802db9a1c88878f29abc652ef1aca70
kessler44 <>
2008-06-18 01:12:54 +0000

    BUG FIX: updated the admin create user to use the validator code..otherwise it wasn't returning to the form with data & errors when the save action failed.

--- modules/administrationmodule/actions/umgr_saveuser.php
+++ modules/administrationmodule/actions/umgr_saveuser.php
@@ -37,36 +37,23 @@ if (exponent_permissions_check('user_management',exponent_core_makeLocation('adm
 		$i18n = exponent_lang_loadFile('modules/administrationmodule/actions/umgr_saveuser.php');
 		$_POST['username'] = trim($_POST['username']);
 		if (exponent_users_getUserByName($_POST['username']) != null) {
-			$post = $_POST;
-			unset($post['username']);
-			$post['_formError'] = $i18n['name_taken'];
-			exponent_sessions_set('last_POST',$post);
-			header('Location: ' . $_SERVER['HTTP_REFERER']);
+			unset($_POST['username']);
+	                validator::failAndReturnToForm($i18n['name_taken'], $_POST);
 		} else if ($_POST['pass1'] != $_POST['pass2']) {
-			$post = $_POST;
-			unset($post['pass1']);
-			unset($post['pass2']);
-			$post['_formError'] = $i18n['unmatched_passwords'];
-			exponent_sessions_set('last_POST',$post);
-			header('Location: ' . $_SERVER['HTTP_REFERER']);
+			unset($_POST['pass1']);
+	                unset($_POST['pass2']);
+	                validator::failAndReturnToForm($i18n['unmatched_passwords'], $_POST);
 		} else {
 			$username_error = exponent_security_checkUsername($_POST['username']);
 			$strength_error = exponent_security_checkPasswordStrength($_POST['username'],$_POST['pass1']);
 			
 			if ($username_error != ''){
-				$post = $_POST;
-				unset($post['pass1']);
-				unset($post['pass2']);
-				$post['_formError'] = sprintf($i18n['username_failed'],$username_error);
-				exponent_sessions_set('last_POST',$post);
-				header('Location: ' . $_SERVER['HTTP_REFERER']);
+				unset($_POST['username']);
+		                validator::failAndReturnToForm(sprintf($i18n['username_failed'],$username_error), $_POST);
 			}else if ($strength_error != '') {
-				$post = $_POST;
-				unset($post['pass1']);
-				unset($post['pass2']);
-				$post['_formError'] = sprintf($i18n['strength_failed'],$strength_error);
-				exponent_sessions_set('last_POST',$post);
-				header('Location: ' . $_SERVER['HTTP_REFERER']);
+				unset($_POST['pass1']);
+	                        unset($_POST['pass2']);
+        	                validator::failAndReturnToForm(sprintf($i18n['strength_failed'],$strength_error), $_POST);
 			} else {
 				$u = exponent_users_create($_POST,null);
 				$u = exponent_users_saveProfileExtensions($_POST,$u,true);
