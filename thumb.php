49850a4c615d9feae6a8af54872b46280896692a
kessler44 <>
2008-08-05 23:34:37 +0000

    checking this in so i can access it on another computer...it is currently broke...fix immediatelyvi thumb.php

--- thumb.php
+++ thumb.php
@@ -39,22 +39,62 @@ $quality = isset($_GET['quality']) ? intval($_GET['quality']) : 75;
 $file = BASE.$_GET['file'];
 $thumb = null;
 
+$sizeinfo = exponent_image_sizeinfo($filename);
+if (!is_array($sizeinfo)) exit();
+
+$original = exponent_image_createFromFile($filename,$sizeinfo);
+if (!is_resource($original)) exit();
+
+$w = $_GET['width'];
+$h = $_GET['height'];
+
 if (isset($_GET['constraint'])) {
-	$thumb = exponent_image_scaleToConstraint($file,$_GET['width'],$_GET['height']);
+	$w = $width;
+	$h = ($width / $sizeinfo[0]) * $sizeinfo[1];
+
+	if ($h > $height) { // height is outside
+		$w = ($height / $sizeinfo[1]) * $sizeinfo[0];
+		$h = $height;
+	}
+	//$thumb = exponent_image_scaleToConstraint($file,$_GET['width'],$_GET['height']);
 } else if (isset($_GET['width'])) {
-	$thumb = exponent_image_scaleToWidth($file,intval($_GET['width']));
+	$w = $width;
+	$h = ($width / $sizeinfo[0]) * $sizeinfo[1];
+	//$thumb = exponent_image_scaleToWidth($file,intval($_GET['width']));
 } else if (isset($_GET['height'])) {
-	$thumb = exponent_image_scaleToHeight($file,intval($_GET['height']));
+	$w = ($height / $sizeinfo[1]) * $sizeinfo[0];
+	$h = $height;
+	//$thumb = exponent_image_scaleToHeight($file,intval($_GET['height']));
 } else if (isset($_GET['scale'])) {
-	$thumb = exponent_image_scaleByPercent($file,intval($_GET['scale']) / 100);
+	$scale = intval($_GET['scale']) / 100;
+	$w = $scale * $sizeinfo[0];
+	$h = $scale * $sizeinfo[1];
+	//$thumb = exponent_image_scaleByPercent($file,intval($_GET['scale']) / 100);
 } else if (isset($_GET['square'])) {
- 	$thumb = exponent_image_scaleToSquare($file,intval($_GET['square']));
+ 	//$thumb = exponent_image_scaleToSquare($file,intval($_GET['square']));
 }
+
+//check for cached file
+$thumbpath = BASE.'files/thumbs/';
+$cachefile = $filename."-".$w."x".$h;
+//Check to see if the directory exists.  If not, create the directory structure.
+if (!file_exists($thumbpath)) exponent_files_makeDirectory($thumbpath);
+if (file_exists($thumbpath.$cachefile)) {
+	$fh = fopen($thumbpath.$cachefile,'rb');
+	$img = fread($fh);
+	fclose($fh);
+	header('Content-type: '.$sizeinfo['mime']);
+	echo $img;
+	exit();
+}
+
+$thumb = exponent_image_create($w,$h);
+if (!$thumb) exit();
+exponent_image_copyresized($thumb,$original,0,0,0,0,$w,$h,$sizeinfo[0],$sizeinfo[1]);
 
 $mythumb = getimagesize($file);
 
-if ($mythumb[0] > 0 && $mythumb[1] > 0)
-{
+if ($mythumb[0] > 0 && $mythumb[1] > 0) {
     if (is_resource($thumb)) {
         exponent_image_output($thumb, exponent_image_sizeinfo($file), null, $quality);
     } else {
