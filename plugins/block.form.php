15a33f263d3ea9e43d6ad62e4b88d0a3e937d617
maia-good <maia_good@hotmail.com>
2009-01-21 11:28:09 -0500

    Made functions XHTML compliant.

--- plugins/block.form.php
+++ plugins/block.form.php
@@ -21,7 +21,7 @@ function smarty_block_form($params,$content,&$smarty, &$repeat) {
 	if(empty($content)){
 		$name = isset($params['name']) ? $params['name'] : 'form';
 		$module = isset($params['module']) ? $params['module'] : $smarty->_tpl_vars['__loc']->mod;
-		$method = isset($params['method']) ? $params['method'] : "POST";
+		$method = isset($params['method']) ? $params['method'] : "post";
 		$enctype = isset($params['enctype']) ? $params['enctype'] : 'multipart/form-data';
 
 		echo "<!-- Form Object 'form' -->\r\n";
@@ -32,19 +32,19 @@ function smarty_block_form($params,$content,&$smarty, &$repeat) {
 		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'external/jscalendar/lang/calendar-en.js"></script>'."\r\n";
 		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'external/jscalendar/calendar-setup.js"></script>'."\r\n";
 		echo '<script type="text/javascript" src="'.PATH_RELATIVE.'js/PopupDateTimeControl.js"></script>'."\r\n";
-		echo '<form id="'.$name.'" name="'.$name.'" class="'.$params['class'].'" method="'.$method.'" action="'.URL_FULL.'index.php" enctype="'.$enctype.'">'."\r\n";
+		echo '<form id="'.$name.'" class="'.$params['class'].'" method="'.$method.'" action="'.URL_FULL.'index.php" enctype="'.$enctype.'">'."\r\n";
 		echo '<input type="hidden" name="module" id="module" value="'.$module.'" />'."\r\n";
 		echo '<input type="hidden" name="src" id="src" value="'.$smarty->_tpl_vars['__loc']->src.'" />'."\r\n";
 		echo '<input type="hidden" name="int" id="int" value="'.$smarty->_tpl_vars['__loc']->int.'" />'."\r\n";
 		echo isset ($params['ajax']) ? '<input type="hidden" name="ajax_action" value="1" />'."\r\n" : '';
 		if (isset($params['action']))  echo '<input type="hidden" name="action" id="action" value="'.$params['action'].'" />'."\r\n";
-	
+
 		//echo the innards
-	}else{	
-		echo $content;	
+	}else{
+		echo $content;
 		echo '</form>';
 	}
-	
+
 }
 
 ?>
