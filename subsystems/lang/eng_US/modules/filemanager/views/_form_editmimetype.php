d79407c90a4c88a6fa519f52d517dbde85b54ef8
Leonard Smith <leonards@kenosisdesigns.com>
2009-05-27 14:56:11 -0500

    Changed wording for mimetype editor to reflect the new process for uploading mime icons.

--- subsystems/lang/eng_US/modules/filemanager/views/_form_editmimetype.php
+++ subsystems/lang/eng_US/modules/filemanager/views/_form_editmimetype.php
@@ -3,7 +3,7 @@
 return array(
 	'form_title_edit'=>'Edit MIME Type',
 	'form_title_new'=>'New MIME Type',
-	'form_header'=>'Clicking on the "(change)" link will bring up the icon picker, which allows you to assign an icon to this MIME type.',
+	'form_header'=>'Here you can upload an icon for this mimetype.',
 );
 
 ?>
