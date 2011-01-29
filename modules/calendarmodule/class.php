8398b2b319f8199c0e8a285f9d8887dc9dbc1425
Dave <dleffler@hughes.net>
2011-01-29 12:35:18 -0500

    Fix Calendar RSS item time to reflect local time vs UTC

--- modules/calendarmodule/class.php
+++ modules/calendarmodule/class.php
@@ -64,7 +64,7 @@ class calendarmodule {
 			$rss_item = new FeedItem();
 			$rss_item->title = $item->title;
 			$rss_item->description = $item->body;
-			$rss_item->date = $item->eventstart;
+			$rss_item->date = date('r', $item->eventstart);
 //          $rss_item->date = date('r', $item->posted);
 //			$rss_item->link = "http://".HOSTNAME.PATH_RELATIVE."index.php?module=calendarmodule&action=view&id=".$item->id."&src=".$loc->src;
 			$rss_item->link = exponent_core_makeLink(array('module'=>'calendarmodule', 'action'=>'view', 'id'=>$item->id));
