91ef08292d5b87c706516a8494edf0e3b71c040d
dleffler <dleffler@hughes.net>
2011-04-21 14:51:02 -0400

    Adds missing language files for Listing module

---
+++ subsystems/lang/eng_US/modules/listingmodule/actions/edit_listing.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+	'no_category'=>'Not Categorized',
+
+);
+
+?>
---
+++ subsystems/lang/eng_US/modules/listingmodule/views/_form_edit.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'form_title'=>'Edit Listing Description',
+	'form_header'=>'Simply type the description of the listing you want to appear on the site into the WYSIWYG editor below, and click Save.',
+);
+
+?>
