c2f094c359b0e2483b52c0bf492aaca6dbf932c6
dleffler <dleffler@hughes.net>
2022-11-26 10:54:54 -0500

    update version to 0.99.0beta3

--- exponent_version.php
+++ exponent_version.php
@@ -44,7 +44,7 @@ if (!defined('EXPONENT_VERSION_MAJOR')) {
 	/* exdoc
 	 * This is the date that this version of Exponent was exported from BZR and built.
 	 */
-	define('EXPONENT_VERSION_BUILDDATE','1314960719');
+	define('EXPONENT_VERSION_BUILDDATE','1669478030');
 	/* exdoc
 	 * This specifies the type of release, either 'alpha','beta','rc' or 'ga' (for stable).
 	 */
@@ -53,7 +53,7 @@ if (!defined('EXPONENT_VERSION_MAJOR')) {
 	 * This number is bumped each time a distribution of a single version is
 	 * released.  For instance, the 3rd beta has an version type iteration of 3.
 	 */
-	define('EXPONENT_VERSION_ITERATION','2'); // only applies to betas/alphas / rcs
+	define('EXPONENT_VERSION_ITERATION','3'); // only applies to betas/alphas / rcs
 }
 
 ?>
